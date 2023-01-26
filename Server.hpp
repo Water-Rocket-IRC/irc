27142d70ec1b0788eec89f92c4cebbcc012d432c
jinwoole <jinwoole@student.42seoul.kr>
2023-01-26 11:19:01 +0900

    beginning

---
+++ Server.hpp
@@ -0,0 +1,26 @@
+#ifndef SERVERHPP
+#define SERVERHPP
+
+
+#include "Receiver.hpp"
+
+class Server
+{
+	private:
+		Server();
+		~Server();
+	public:
+
+};
+
+Server::Server()
+{
+	
+}
+
+Server::~Server()
+{
+	
+}
+
+#endif
