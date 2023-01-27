25d442306e50883a3c1a36c99a229595fbd280f1
jinwoole <jinwoole@student.42seoul.kr>
2023-01-27 11:07:48 +0900

    received, Users, User

--- Users.hpp
+++ Users.hpp
@@ -1,9 +1,30 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+/// @brief 개별 클라이언트에서 접속한 유저의 정보
+struct User
+{
+	std::string	nickname_;
+	std::string username_;
+	int			client_sock_;
+	int			mod;
+	//그 외 기타등등
+};
+
+
+/// @brief 유저들을 관리하고, sender에게 적절한 응답을 요청한다.
 class Users
 {
 	private:
-
+		std::vector<User> user_list_;
 	public:
+		Users();
 
 };
+
+Users::Users()
+{
+	
+}
