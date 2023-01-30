364f42f68b17633b673a23e3aa6f26ff2e766433
change-challenge <dog941128@hotmail.com>
2023-01-31 04:02:11 +0900

    feat: user command done

--- Users.hpp
+++ Users.hpp
@@ -31,6 +31,8 @@ class Users
 		void print_all_user(); //debug
 };
 
+
+// ident 즉 socket을 이용해 지금 명령어 친 user가 누군 지 알아낸다. 
 std::vector<user>::iterator	Users::search_user_by_ident(uintptr_t sock)
 {
 	std::vector<user>::iterator	it;
@@ -45,6 +47,10 @@ std::vector<user>::iterator	Users::search_user_by_ident(uintptr_t sock)
 	return (it);
 }
 
+// nick이 중복되는 지 체크하는 함수
+// pair을 쓴 이유
+// 중복되면 it는 중복된 user에서의 it와 bool값은 true를 가지고 나간다.
+// 중복되지 않으면 it는 it는 end()와 bool값은 false를 가지고 나간다. 
 std::pair<std::vector<user>::iterator, bool>	Users::check_dup_nick(std::string nickname)
 {
     std::pair<std::vector<user>::iterator, bool> pair;
@@ -61,6 +67,7 @@ std::pair<std::vector<user>::iterator, bool>	Users::check_dup_nick(std::string n
 	return (pair);
 }
 
+// nick을 실행하는 함수
 Udata	Users::command_nick(std::stringstream &line_ss, struct kevent event)
 {
 	std::vector<Udata>	ret;
@@ -68,18 +75,13 @@ Udata	Users::command_nick(std::stringstream &line_ss, struct kevent event)
 	line_ss >> nickname;
 
 	// 경우의 수 2 가지 
-	// 1. 중복 O -> 안된다고 적어줌 
+	// 1. 중복 O -> 안된다고 적어줌
 	// 2. 중복 X -> 된다고 적어줌
 	std::pair<std::vector<user>::iterator, bool> pair;
 	std::vector<user>::iterator my_user;
 
 	pair = check_dup_nick(nickname);
 	my_user = search_user_by_ident(event.ident);
-	if (my_user == user_list_.end()) // 처음 들어온 경우
-	{
-		my_user->nickname_ = nickname;
-		my_user->event = event;
-	}
 	if (pair.second) // 중복 O
 	{
 		return (Sender::nick_error_message(*my_user, nickname))
@@ -100,104 +102,7 @@ Udata	Users::command_nick(std::stringstream &line_ss, struct kevent event)
 	}
 }
 
-// user Users::search_user_nick(std::string nick)
-// {
-// 	std::vector<user>::iterator it;
-// 	user usr;
-
-// 	for (it = user_list_.begin(); it != user_list_.end(); it++)
-// 	{
-// 		if (it->nickname_ == nick)
-// 			return *it;
-// 	}
-// 	usr.client_sock_ = -433;
-// 	return usr;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-https://datatracker.ietf.org/doc/html/rfc1459#section-4.1.3
-에 따르면, 이건 서버에 처음 접속할때 사용되는 명령어다. NICK과 USER 모두 접수되어야, 서버에 레지스터 된 것이다.
-중간에 유저 명령어 전송이 가능한지 확인해야 하고, 그에 따라 처리해야 한다.
-*/
-Udata	Users::command_user(std::stringstream &line_ss, uintptr_t sock)
-{
-	std::vector<user>::iterator my_user = search_user_by_ident(sock);
-	std::string username, username, hostname, realname;
-	
-	line_ss >> username >> hostname >> hostname >> realname;
-	
-	
-	if (my_user->nickname_.empty())
-		break;
-	realname.erase(0, 1); //prefix 제거 ':' <- 이거 제거
-
-	my_user->username_ = username;
-	my_user->hostname_ = hostname;
-	my_user->hostname_ = hostname;
-	my_user->realname_ = realname;
-
-	return(Sender::welcome_message(my_user));
-}
-
-
-/// @brief kqueue의 event를 통해 서버에 메시지를 전송한 유저를 식별하는 함수
-/// @param event 서버가 listen한 event
-/// @return user
-// user Users::search_user_event(struct kevent event)
-// {
-// 	std::vector<user>::iterator it;
-// 	user usr;
-
-// 	for (it = user_list_.begin(); it != user_list_.end(); it++)
-// 	{
-// 		if (it->event.ident == event.ident)
-// 			return *it;
-// 	}
-// 	std::cout << "Error : Unknown User accessed to the server" << std::endl;
-// 	return usr;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// quit을 실행하는 함수
 Udata	Users::command_quit(std::stringstream &line_ss, uintptr_t sock)
 {
 	std::vector<user>::iterator	my_user;
@@ -215,28 +120,35 @@ Udata	Users::command_quit(std::stringstream &line_ss, uintptr_t sock)
 		}
 	}
 
-	return (Sender::quit_channel_message(my_user, leave_message));
+	return (Sender::quit_channel_message(*my_user, leave_message));
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// user를 실행하는 함수 
+Udata	Users::command_user(std::stringstream &line_ss, uintptr_t sock)
+{
+	std::vector<user>::iterator my_user = search_user_by_ident(sock);
+	std::string username, username, hostname, realname;
+	
+	line_ss >> username >> hostname >> hostname >> realname;
+	
+	if (my_user->nickname_.empty()) // nick을 안달고 온 것! 오류사항
+		return (Udata());
+	if (my_user->username_.empty())
+	{
+		realname.erase(0, 1); //prefix 제거 ':' <- 이거 제거
+		my_user->username_ = username;
+		my_user->hostname_ = hostname;
+		my_user->hostname_ = hostname;
+		my_user->realname_ = realname;
+		return(Sender::welcome_message(*my_user));
+	}
+	realname.erase(0, 1); //prefix 제거 ':' <- 이거 제거
+	my_user->username_ = username;
+	my_user->hostname_ = hostname;
+	my_user->hostname_ = hostname;
+	my_user->realname_ = realname;
+	return (Udata());
+}
 
 bool    Users::check_dup_username(std::string username)
 {
@@ -249,11 +161,6 @@ bool    Users::check_dup_username(std::string username)
 	}
 	return (false);
 }
-
-
-
-
-
 
 //debug 함수
 void Users::print_all_user()
